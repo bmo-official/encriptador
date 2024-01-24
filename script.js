825e6e10805b8575c3229ab7813c7342ed538037
bmo-official <157325223+bmo-official@users.noreply.github.com>
2024-01-24 15:18:11 -0600

    Add files via upload

--- script.js
+++ script.js
@@ -1,16 +1,27 @@
 function encriptarTexto() {
-    // Obtener el valor del input de texto
     var textoOriginal = document.getElementById('texto').value;
-
-    // Lógica de encriptación (aquí puedes implementar tu propia lógica de encriptación)
     var textoEncriptado = encriptarAlgunModo(textoOriginal);
+    document.getElementById('resultado').innerText = textoEncriptado;
+}
 
-    // Mostrar el resultado en la página
-    document.getElementById('textoEncriptado').innerText = textoEncriptado;
+function desencriptarTexto() {
+    var textoEncriptado = document.getElementById('texto').value;
+    var textoDesencriptado = desencriptarAlgunModo(textoEncriptado);
+    document.getElementById('resultado').innerText = textoDesencriptado;
+}
+
+function vaciarCajaDeTexto() {
+    document.getElementById('texto').value = '';
 }
 
 function encriptarAlgunModo(texto) {
     // Implementa tu propia lógica de encriptación aquí
+    // Este es un ejemplo muy básico de reversión de cadena
+    return texto.split('').reverse().join('');
+}
+
+function desencriptarAlgunModo(texto) {
+    // Implementa tu propia lógica de desencriptación aquí
     // Este es un ejemplo muy básico de reversión de cadena
     return texto.split('').reverse().join('');
 }
